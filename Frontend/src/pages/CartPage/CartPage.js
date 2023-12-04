a120e60b38863fa1d579c733dcd4af1dc3f75506
unknown <dangkhoa141003@gmail.com>
2023-12-04 14:26:36 +0700

    UI 404 Page & Checkout Page & Cart Page

--- Frontend/src/pages/CartPage/CartPage.js
+++ Frontend/src/pages/CartPage/CartPage.js
@@ -1,11 +1,140 @@
-import React from 'react'
+import React, { useState } from "react";
+import { List, Row, Card, Button, InputNumber, Checkbox } from "antd";
+
+const onChange = (e) => {
+  console.log(`checked = ${e.target.checked}`);
+};
+
+const datacart = [
+  {
+    name: "sach 1",
+    dongia: "100.000đ",
+    soluong: "1",
+    sotien: "100.000đ",
+  },
+  {
+    name: "sach 2",
+    dongia: "200.000đ",
+    soluong: "1",
+    sotien: "200.000đ",
+  },
+  {
+    name: "sach 3",
+    dongia: "300.000đ",
+    soluong: "1",
+    sotien: "200.000đ",
+  },
+];
 
 function CartPage() {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "sach 1",
+      dongia: "100.000đ",
+      soluong: "1",
+      sotien: "100.000đ",
+    },
+    {
+      id: 2,
+      name: "sach 2",
+      dongia: "200.000đ",
+      soluong: "1",
+      sotien: "200.000đ",
+    },
+    {
+      id: 3,
+      name: "sach 3",
+      dongia: "300.000đ",
+      soluong: "1",
+      sotien: "200.000đ",
+    },
+  ]);
+
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  function checkboxHandler(e) {
+    let isSelected = e.target.checked;
+    let value = parseInt(e.target.value);
+
+    if (isSelected) {
+      setSelectedItems([...selectedItems, value]);
+    } else {
+      setSelectedItems((prevData) => {
+        return prevData.filter((id) => {
+          return id !== value;
+        });
+      });
+    }
+  }
+
+  function checkAllHandler() {
+    if (data.length === selectedItems.length) {
+      setSelectedItems([]);
+    } else {
+      const postIds = data.map((item) => {
+        return item.id;
+      });
+
+      setSelectedItems(postIds);
+    }
+  }
   return (
-    <div>
-      CartPage
+    <div className="container">
+      <div className="results">
+        <List>
+          <List.Item>
+            <List.Item>
+              {
+                <div>
+                  <Checkbox onClick={checkAllHandler}>
+                    {data.length === selectedItems.length
+                      ? "Hủy chọn tất cả"
+                      : "Chọn tất cả"}
+                  </Checkbox>
+                  <h3>Sản phẩm</h3>
+                </div>
+              }
+            </List.Item>
+            <List.Item>{<div>đơn giá</div>}</List.Item>
+            <List.Item>{<div>số lượng</div>}</List.Item>
+            <List.Item>{<div>thành tiền</div>}</List.Item>
+          </List.Item>
+        </List>
+      </div>
+      <div>
+        {data.map((item, index) => (
+          <div className="card" key={index}>
+            <Card>
+              <Row>
+                <Checkbox
+                  checked={selectedItems.includes(item.id)}
+                  value={item.id}
+                  onChange={checkboxHandler}
+                />
+
+                <List.Item>{<div>{item.name}</div>}</List.Item>
+                <List.Item>{<div>{item.dongia}</div>}</List.Item>
+                <List.Item>{<div>{item.soluong}</div>}</List.Item>
+                <List.Item>{<div>{item.thanhtien}</div>}</List.Item>
+              </Row>
+            </Card>
+          </div>
+        ))}
+      </div>
+      <div>
+      <List>
+          <List.Item>
+            <h3>Thanh toán</h3>
+          </List.Item>
+          <List.Item>Tổng thanh toán: 130000đ</List.Item>
+          <List.Item>
+            <Button>Mua Hàng</Button>
+          </List.Item>
+        </List>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CartPage
+export default CartPage;
--- Frontend/src/pages/CheckoutPage/index.js
+++ Frontend/src/pages/CheckoutPage/index.js
@@ -1,17 +1,61 @@
-import { Button, Row, Col, List } from "antd";
-import React from "react";
+import {
+  Button,
+  Row,
+  Col,
+  List,
+  Typography,
+  Card,
+  Dropdown,
+  message,
+  Space,
+} from "antd";
+import React, { useState } from "react";
 import { IoLocationSharp } from "react-icons/io5";
+
+const { Paragraph } = Typography;
 
 const data = [
   {
-    title: "LLDT",
+    name: "LLDT",
     phone: "0946854546",
     location:
       "Ktx Đại Học Quốc Gia Tp. Hồ Chí Minh, Khu A, Phường Đông Hòa, Thành Phố Dĩ An, Bình Dương",
   },
 ];
 
+const datacart = [
+  {
+    name: "sach 1",
+    dongia: "100.000đ",
+    soluong: "1",
+    thanhtien: "100.000đ",
+  },
+  {
+    name: "sach 2",
+    dongia: "200.000đ",
+    soluong: "1",
+    thanhtien: "200.000đ",
+  },
+  {
+    name: "sach 3",
+    dongia: "300.000đ",
+    soluong: "1",
+    thanhtien: "200.000đ",
+  },
+];
+
+const items = [
+  { key: "1", label: "discount 30%" },
+  { key: "2", label: "discount 40%" },
+  { key: "3", label: "discount 60%" },
+];
+
+const onClick = ({ key }) => {
+  message.info(`Click on item ${key}`);
+};
+
 function CheckoutPage() {
+  const [editableStr, setEditableStr] = useState();
   return (
     <div>
       <div className="location">
@@ -25,15 +69,69 @@ function CheckoutPage() {
           dataSource={data}
           renderItem={(item) => (
             <List.Item>
-              <List.Item.Meta
-                title={item.title}
-                phone={<div>{item.phone}</div>}
-                location={<div>{item.location}</div>}
-                changebtn={<Button>Thay Đổi</Button>}
-              />
+              <List.Item>{<div>Tên người dùng: {item.name}</div>}</List.Item>
+              <List.Item>{<div>Phone: {item.phone}</div>}</List.Item>
+              <List.Item>
+                {
+                  <Paragraph editable={{ onChange: setEditableStr }}>
+                    {item.location}
+                  </Paragraph>
+                }
+              </List.Item>
             </List.Item>
           )}
         ></List>
+      </div>
+      <div>
+        <List>
+          <List.Item>
+            <List.Item>{<h3>Sản phẩm</h3>}</List.Item>
+            <List.Item>{<div>đơn giá</div>}</List.Item>
+            <List.Item>{<div>số lượng</div>}</List.Item>
+            <List.Item>{<div>thành tiền</div>}</List.Item>
+          </List.Item>
+        </List>
+        <List
+          dataSource={datacart}
+          renderItem={(item) => (
+            <List.Item>
+              <Card>
+                <Row>
+                  <List.Item>{<div>{item.name}</div>}</List.Item>
+                  <List.Item>{<div>{item.dongia}</div>}</List.Item>
+                  <List.Item>{<div>{item.soluong}</div>}</List.Item>
+                  <List.Item>{<div>{item.thanhtien}</div>}</List.Item>
+                </Row>
+              </Card>
+            </List.Item>
+          )}
+        ></List>
+      </div>
+      <div>
+        <h3>Voucher</h3>
+        <Dropdown
+          menu={{
+            items,
+            onClick,
+          }}
+        >
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>Hover me</Space>
+          </a>
+        </Dropdown>
+      </div>
+      <div>
+        <List>
+          <List.Item>
+            <h3>Phương thức thanh toán</h3>
+          </List.Item>
+          <List.Item>Tổng tiền hàng: 100000đ</List.Item>
+          <List.Item>Phí vận chuyển: 30000đ</List.Item>
+          <List.Item>Tổng thanh toán: 130000đ</List.Item>
+          <List.Item>
+            <Button>Thanh toán</Button>
+          </List.Item>
+        </List>
       </div>
     </div>
   );
